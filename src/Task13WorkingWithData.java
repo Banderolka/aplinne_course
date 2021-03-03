a3ef7a6fd8c22bf4fa5db95d350f52a06eb44e48
Banderolka <bothisava@gmail.com>
2021-03-03 16:01:27 +0300

    добавил класс Task13WorkingWithData  - решение задачи 13 добавил коменты

--- src/Task13WorkingWithData.java
+++ src/Task13WorkingWithData.java
@@ -7,13 +7,14 @@ public class Task13WorkingWithData {
     public static void main(String[] args) {
         String str = "I love java 8 Я люблю java 14 core1";
 
+        //задаю регулятное выражение
        Pattern pattern = Pattern.compile("[A-Za-z]{1,}\\s");
        Matcher matcher = pattern.matcher(str);
 
 
-        List<String> stringList = new ArrayList<>();
+        List<String> stringList = new ArrayList<>(); // создаю лист для хранения отсортировных данных
         while (matcher.find()) {
-           stringList.add(str.substring(matcher.start(), matcher.end()));
+           stringList.add(str.substring(matcher.start(), matcher.end())); // сортирую данные соглачно условию и сохраняю в лист
        }
 
        System.out.println(stringList.size());
